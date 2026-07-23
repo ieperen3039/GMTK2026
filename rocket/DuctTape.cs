d06dcea8915bdc4da44bfdaba20d46b5227b9e6f
Geert van Ieperen <ieperen3039@outlook.com>
2026-07-23 22:21:03 +0200

    Relative grabbing of objects

--- rocket/DuctTape.cs
+++ rocket/DuctTape.cs
@@ -10,8 +10,9 @@ public partial class DuctTape : Node2D
         FullConnected
     }
 
-    private const float Pull = 100;
     public const float MousePullFactor = 0.1f;
+    public const float SnapSpeed = 200f;
+    public const float SnapDampening = 200f;
 
     public RocketComponent ComponentA { get; private set; } = null;
     private Vector2 anchorA = new();
@@ -30,15 +31,15 @@ public partial class DuctTape : Node2D
 
     public StatusValue Status => ComponentA == null ? StatusValue.Empty : (ComponentB == null ? StatusValue.HalfConnected : StatusValue.FullConnected);
 
-    public void Attach(RocketComponent component, Vector2 globalAttachmentPosition)
+    public void Attach(RocketComponent component, Vector2 localAttachmentPosition)
     {
         switch (Status)
         {
             case StatusValue.Empty:
                 GD.Print("Tape attach A");
                 ComponentA = component;
-                anchorA = globalAttachmentPosition;
-                Vector2 linePoint = ToLocal(ComponentA.ToGlobal(globalAttachmentPosition));
+                anchorA = localAttachmentPosition;
+                Vector2 linePoint = ToLocal(ComponentA.ToGlobal(localAttachmentPosition));
                 graphic.AddPoint(linePoint);
                 graphic.AddPoint(linePoint);
                 return;
@@ -53,7 +54,8 @@ public partial class DuctTape : Node2D
 
                 GD.Print("Tape attach B");
                 ComponentB = component;
-                anchorB = globalAttachmentPosition;
+                anchorB = localAttachmentPosition;
+                length = anchorA.DistanceTo(anchorB);
                 return;
 
             case StatusValue.FullConnected:
@@ -97,8 +99,15 @@ public partial class DuctTape : Node2D
         graphic.SetPointPosition(0, ToLocal(globalAnchorA));
         graphic.SetPointPosition(1, ToLocal(globalAnchorB));
 
-        Vector2 vecAToB = globalAnchorB - globalAnchorA;
-        Vector2 force = vecAToB.Normalized() * Pull;
+        Vector2 globalVelocityA = ComponentA.GlobalTransform.BasisXform(ComponentB.LinearVelocity);
+        Vector2 globalVelocityB = ComponentB.GlobalTransform.BasisXform(ComponentB.LinearVelocity);
+
+        // relative to A
+        Vector2 gap = globalAnchorB - globalAnchorA;
+        Vector2 direction = gap - (gap.Normalized() * length);
+        Vector2 targetVelocity = direction * SnapSpeed;
+        Vector2 velocityDifference = targetVelocity - (globalVelocityB - globalVelocityA);
+        Vector2 force = velocityDifference * SnapDampening;
 
         ComponentA.ApplyForce(force, globalAnchorA - ComponentA.GlobalPosition);
         ComponentB.ApplyForce(-force, globalAnchorB - ComponentB.GlobalPosition);
