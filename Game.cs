397489a53e9e92dc50a8a27eee5e34b0ef7dd617
Geert van Ieperen <ieperen3039@outlook.com>
2026-07-23 10:49:44 +0200

    Overall level handling logic

--- Game.cs
+++ Game.cs
@@ -4,14 +4,30 @@ using System;
 // level-manager
 public partial class Game : Node
 {
-    [Export]
-    private PackedScene[] levels;
+    private PackedScene[] levelScenes;
 
+    private int currentLevelIdx = 0;
+    private Level currentLevel;
 
     public override void _Ready()
     {
-        PackedScene firstLevelScene = ResourceLoader.Load<PackedScene>("res://...");
-        Level firstLevel = firstLevelScene.Instantiate<Level>();
+        levelScenes = [
+            ResourceLoader.Load<PackedScene>("res://levels/level-1/scene.tscn"),
+            ResourceLoader.Load<PackedScene>("res://levels/level-2/scene.tscn")
+        ];
+        
+        Level firstLevel = levelScenes[0].Instantiate<Level>();
         AddChild(firstLevel);
+    }
+
+    void NextLevel()
+    {
+        // TODO add fader
+
+        RemoveChild(currentLevel);
+        PackedScene packedScene = levelScenes[currentLevelIdx++];
+        currentLevel = packedScene.Instantiate<Level>();
+        currentLevel.OnNextLevel += NextLevel;
+        AddChild(currentLevel);
     }
 }
--- levels/Level.cs
+++ levels/Level.cs
@@ -5,5 +5,43 @@ using System.Collections.Generic;
 // contains whats in a level
 public partial class Level : Node
 {
+    [Signal]
+    public delegate void OnNextLevelEventHandler();
+
+    private PackedScene levelCompleteScene;
     private List<RocketComponent> parts;
+    private Camera2D camera;
+
+    public override void _Ready()
+    {
+        levelCompleteScene = ResourceLoader.Load<PackedScene>("uid://s62hk0dts0pl");
+
+        camera = GetNode<Camera2D>("Camera2D");
+        Node rocketParts = GetNode<Node>("RocketComponents");
+        foreach (Node child in rocketParts.GetChildren())
+        {
+            if (child is RocketComponent part)
+            {
+                parts.Add(part);
+            }
+        }
+    }
+
+    // attach camera to largest component tree, activate all engines
+    private void OnCountdownZero()
+    {
+        foreach (RocketComponent part in parts)
+        {
+            // ...
+        }
+    }
+
+    private void OnLevelComplete()
+    {
+        LevelComplete levelCompleteScreen = levelCompleteScene.Instantiate<LevelComplete>();
+        // chain level complete signal to this level complete signal
+        levelCompleteScreen.OnNextLevel += () => EmitSignal(SignalName.OnNextLevel);
+        AddChild(levelCompleteScreen);
+    }
+
 }
