f330525661a4715227b0e662116d5e7c6fab6d7f
Geert van Ieperen <ieperen3039@outlook.com>
2026-07-26 11:57:30 +0200

    Rework with new art

--- rocket/rocket-components/thruster/Thruster.cs
+++ rocket/rocket-components/thruster/Thruster.cs
@@ -4,23 +4,25 @@ using System.Collections.Generic;
 
 public partial class Thruster : RocketComponent
 {
+    [Export]
+    private Vector2 InitialParticleVelocity = new(0, 500);
+
     private List<CpuParticles2D> _particles = [];
-    // private Sprite2D _flameSprite;
     public override void _Ready()
     {
         base._Ready();
 
-        foreach(Node node in GetNode<Node2D>("ExhaustParticles").GetChildren())
+        foreach (Node node in GetNode<Node2D>("ExhaustParticles").GetChildren())
         {
             if (node is CpuParticles2D particle)
             {
                 _particles.Add(particle);
                 particle.Emitting = false;
-                GD.Print("Disable particle");
             }
         }
-        // _flameSprite = GetNode<Sprite2D>("%ExhaustFlame");
-        // _flameSprite.Hide();
+
+        // TODO remove
+        ActivateThruster();
     }
 
     public void ActivateThruster()
@@ -31,13 +33,10 @@ public partial class Thruster : RocketComponent
             thruster.SetActivationThrustFactor();
         }
 
-        
         // Enable the visuals
-        // _flameSprite.Show();
-        foreach(CpuParticles2D particle in _particles)
+        foreach (CpuParticles2D particle in _particles)
         {
             particle.Emitting = true;
         }
     }
-
 }
